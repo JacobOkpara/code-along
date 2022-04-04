2ddeb1eb9ee92e44504337ae125fab27e11c49e6
Jacob Okpara <okparajacob@gmail.com>
2022-04-04 15:00:16 +0100

    react-component-8

---
+++ src/pages/Home.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section>
+      <h1>Welcome to the Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum
+        aliquid dicta dolore suscipit ullam consequuntur dolorem vel. Fugit
+        incidunt, voluptate vero numquam laborum quibusdam eius error! Dicta,
+        distinctio consequuntur? Ipsam harum quis quo iusto dolores quos alias
+        nostrum dolorem natus facere itaque reiciendis excepturi asperiores id
+        sequi, nam aperiam vero ducimus provident vitae hic, accusantium
+        tempore! Nesciunt, quibusdam architecto. Fugiat, velit reprehenderit!
+        Tenetur commodi ratione amet voluptatum ab eos perspiciatis voluptatibus
+        sint a atque, omnis earum nisi ducimus error maxime autem neque quis
+        quas culpa consectetur saepe soluta fuga.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum
+        aliquid dicta dolore suscipit ullam consequuntur dolorem vel. Fugit
+        incidunt, voluptate vero numquam laborum quibusdam eius error! Dicta,
+        distinctio consequuntur? Ipsam harum quis quo iusto dolores quos alias
+        nostrum dolorem natus facere itaque reiciendis excepturi asperiores id
+        sequi, nam aperiam vero ducimus provident vitae hic, accusantium
+        tempore! Nesciunt, quibusdam architecto. Fugiat, velit reprehenderit!
+        Tenetur commodi ratione amet voluptatum ab eos perspiciatis voluptatibus
+        sint a atque, omnis earum nisi ducimus error maxime autem neque quis
+        quas culpa consectetur saepe soluta fuga.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum
+        aliquid dicta dolore suscipit ullam consequuntur dolorem vel. Fugit
+        incidunt, voluptate vero numquam laborum quibusdam eius error! Dicta,
+        distinctio consequuntur? Ipsam harum quis quo iusto dolores quos alias
+        nostrum dolorem natus facere itaque reiciendis excepturi asperiores id
+        sequi, nam aperiam vero ducimus provident vitae hic, accusantium
+        tempore! Nesciunt, quibusdam architecto. Fugiat, velit reprehenderit!
+        Tenetur commodi ratione amet voluptatum ab eos perspiciatis voluptatibus
+        sint a atque, omnis earum nisi ducimus error maxime autem neque quis
+        quas culpa consectetur saepe soluta fuga.
+      </p>
+    </section>
+  );
+};
+
+export default Home;
